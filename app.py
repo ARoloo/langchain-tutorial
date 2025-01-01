d8958c097fe495a7f21e2ed32a3e5c6fc42d23e3
ARoolo <kontakt@pfeiferv.de>
2025-01-01 20:45:50 +0100

    Add app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(
+    api_key='YOUR_API_KEY',
+    model='gpt-4o-mini',
+    temperature=0.7
+)
+response = llm.invoke("Hi, wie geht es dir?")
+print(response)
